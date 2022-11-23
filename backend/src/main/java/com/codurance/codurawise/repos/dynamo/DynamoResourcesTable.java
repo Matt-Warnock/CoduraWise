f905edf107d4417976a7db5d73ad06d66a6137c4
tiagobica <tiago.bica@codurance.com>
2022-11-23 14:14:40 +0000

    Backend skeleton - running lambda integrated with DynamoDB.

--- backend/src/main/java/com/codurance/codurawise/repos/dynamo/DynamoResourcesTable.java
+++ backend/src/main/java/com/codurance/codurawise/repos/dynamo/DynamoResourcesTable.java
@@ -4,18 +4,17 @@ import com.codurance.codurawise.model.Resource;
 import com.codurance.codurawise.repos.ResourcesRepository;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
-import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
 import software.amazon.awssdk.services.dynamodb.model.ScanRequest;
 import software.amazon.awssdk.services.dynamodb.model.ScanResponse;
 
 import java.util.Collection;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class DynamoResourcesTable implements ResourcesRepository {
 
     private final DynamoDbClient client;
     private final String tableName;
+    private final ResourceConversion resourceConversion = new ResourceConversion();
 
     public DynamoResourcesTable(String region, String tableName) {
         client = DynamoDbClient.builder().region(Region.of(region)).build();
@@ -31,14 +30,7 @@ public class DynamoResourcesTable implements ResourcesRepository {
         ScanResponse response = client.scan(scanRequest);
 
         // transform list of Map<String,Attribute> into set of Resource
-        return response.items().stream().map(this::convert).collect(Collectors.toSet());
+        return response.items().stream().map(resourceConversion::convert).collect(Collectors.toSet());
     }
 
-    protected Resource convert(Map<String, AttributeValue> values) {
-        Resource resource = new Resource();
-        resource.setId(Integer.parseInt(values.get("Id").n()));
-        resource.setTitle(values.get("Title").s());
-        resource.setLink(values.get("Link").s());
-        return resource;
-    }
 }
---
+++ backend/src/main/java/com/codurance/codurawise/repos/dynamo/ResourceConversion.java
@@ -0,0 +1,17 @@
+package com.codurance.codurawise.repos.dynamo;
+
+import com.codurance.codurawise.model.Resource;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+
+import java.util.Map;
+
+public class ResourceConversion {
+
+    protected Resource convert(Map<String, AttributeValue> values) {
+        Resource resource = new Resource();
+        resource.setId(Integer.parseInt(values.get("Id").n()));
+        resource.setTitle(values.get("Title").s());
+        resource.setLink(values.get("Link").s());
+        return resource;
+    }
+}
