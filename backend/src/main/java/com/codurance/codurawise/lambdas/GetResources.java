fd3c433a9a2961a4bb8e3cf1d5d4b8b5b7128f52
timcastillogill <timgillviola@gmail.com>
2022-11-23 12:04:02 +0000

    added MAP to handleRequest and updated return type

--- backend/src/main/java/com/codurance/codurawise/lambdas/GetResources.java
+++ backend/src/main/java/com/codurance/codurawise/lambdas/GetResources.java
@@ -5,9 +5,10 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.util.HashMap;
 import java.util.Map;
 
-public class GetResources implements RequestHandler<Map<String,String>, String> {
+public class GetResources implements RequestHandler<Map<String, String>, Map<String, String>> {
 
     private static final String REGION_PROPERTY = "AWS_REGION";
     private static final String TABLE_NAME_PROPERTY = "TABLE_NAME";
@@ -21,9 +22,12 @@ public class GetResources implements RequestHandler<Map<String,String>, String> 
     }
 
     @Override
-    public String handleRequest(Map<String,String> input, Context context) {
+    public Map<String, String> handleRequest(Map<String,String> input, Context context) {
 
-        return gson.toJson("200 OK");
+        Map<String, String> response = new HashMap<>();
+        response.put("200", "OK");
+        return response;
+//        return gson.toJson("200 OK");
     }
 
 }
