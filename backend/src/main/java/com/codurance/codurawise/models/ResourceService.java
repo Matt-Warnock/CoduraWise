b6c2b56640227fa4806d8b5ac6cd2f143bd43cec
MattW <mattnwarnock@yahoo.co.uk>
2022-11-28 14:57:12 +0000

    Build test for service all resorces

---
+++ backend/src/main/java/com/codurance/codurawise/models/ResourceService.java
@@ -0,0 +1,13 @@
+package com.codurance.codurawise.models;
+
+import com.codurance.codurawise.repos.dynamo.ResourcesDynamoRepository;
+
+import java.util.Collection;
+
+public class ResourceService {
+  public ResourceService(ResourcesDynamoRepository repository) {
+  }
+
+  public Collection<Resource> getAll() {
+  }
+}
---
+++ backend/src/test/java/com/codurance/codurawise/models/ResourceServiceTest.java
@@ -0,0 +1,33 @@
+package com.codurance.codurawise.models;
+
+import com.codurance.codurawise.repos.dynamo.ResourcesDynamoRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class ResourceServiceTest {
+  @Mock ResourcesDynamoRepository repository;
+
+  @Test
+  public void foo() {
+    Collection<Resource> repositoryCollection = new ArrayList<>();
+    ResourceService resourceService = new ResourceService(repository);
+
+    given(repository.getAllResources()).willReturn(repositoryCollection);
+
+    Collection result = resourceService.getAll();
+
+    verify(repository).getAllResources();
+    assertThat(result).isEqualTo(repositoryCollection);
+  }
+}
