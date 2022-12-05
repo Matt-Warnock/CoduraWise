f146ec76a0c5bd3fe6f82191242167c60ec2080c
tiagobica <tiago.bica@codurance.com>
2022-12-05 16:14:02 +0000

    Start mocking context.

--- frontend/src/components/ResourceList/ResourceList.test.tsx
+++ frontend/src/components/ResourceList/ResourceList.test.tsx
@@ -1,12 +1,22 @@
+import React from "react";
 import { render, screen } from "@testing-library/react";
 import ResourceList from "./ResourceList";
-import React from "react";
 import { mockResources } from "../../mocks/mocks";
+import ResourcesContextProvider from "../../store/ResourcesContext";
 
+const mockUseContext = jest.fn().mockImplementation(() => ({
+  filterMediaTypes: [],
+  setFilterMediaTypes: [],
+}));
+
+React.useContext = mockUseContext;
 describe("given the resource list", () => {
   describe("when it receives resource data", () => {
     test("then the titles match the data", () => {
-      render(<ResourceList resources={mockResources} />);
+      render(<ResourcesContextProvider>
+          <ResourceList resources={mockResources} />
+        </ResourcesContextProvider>
+      );
 
       const title1 = screen.getByRole("heading", { name: "Java" });
       expect(title1).toBeInTheDocument();
@@ -19,7 +29,10 @@ describe("given the resource list", () => {
     });
 
     test("then the url match the data", () => {
-      render(<ResourceList resources={mockResources} />);
+      render(<ResourcesContextProvider>
+        <ResourceList resources={mockResources} />
+      </ResourcesContextProvider>
+    );
 
       const url1 = screen.getByRole("link", { name: "Java" });
       expect(url1).toHaveAttribute("href", mockResources[0].link);
