1cb496f64bd6c359f4c37a0c838626f3cdbafd8d
davidajsousa <davidajsousa@hotmail.com>
2022-12-07 14:29:27 +0000

    added SearchBar

---
+++ frontend/src/components/SearchBar/SearchBar.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import ContextRouterMock from "../../mocks/contextRouterMock";
+import SearchBar from "./SearchBar";
+
+describe("given the searchbar", () => {
+  test("should render with the searchbar", () => {
+    render(
+      <ContextRouterMock>
+        <SearchBar />
+      </ContextRouterMock>,
+    );
+
+    const form = screen.getByRole("form");
+    expect(form).toBeInTheDocument();
+  });
+});
+
--- frontend/src/components/SearchBar/SearchBar.tsx
+++ frontend/src/components/SearchBar/SearchBar.tsx
@@ -2,7 +2,24 @@ import React from "react";
 import "./SearchBar.scss";
 
 const SearchBar = () => {
-  return <section role="search"></section>;
+  const submitSearch = () => {
+    console.log("test");
+  };
+  debugger;
+  return (
+    <section role="search">
+      <form onSubmit={submitSearch}>
+        <input
+          type="search"
+          name="whatever"
+          value="hello world"
+          onChange={submitSearch}
+        />
+        <button type="submit">search</button>
+      </form>
+    </section>
+  );
 };
 
 export default SearchBar;
+
