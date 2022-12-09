6c1878f92485ab64f047ffda6f7053f78b2aa362
Tim <61849640+timcastillogill@users.noreply.github.com>
2022-12-09 09:48:12 +0000

    removed console log

--- frontend/src/pages/SearchResultsPage/SearchResultsPage.tsx
+++ frontend/src/pages/SearchResultsPage/SearchResultsPage.tsx
@@ -2,22 +2,22 @@ import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import ListingWithFilter from "../../components/ListingWithFilter/ListingWithFilter";
 import useResources from "../../store/hooks/useResources";
-import './SearchResultsPage.scss'
+import "./SearchResultsPage.scss";
 
 const SearchResultsPage = () => {
   const { resources, searchByTitleAndTag } = useResources();
   const { text } = useParams();
-  
-  console.log('page', text)
 
   useEffect(() => {
-   text ? searchByTitleAndTag(text, text) : [];
+    text ? searchByTitleAndTag(text, text) : [];
   }, [text]);
 
-  return (<section className="search-result-page">
-    <h3>Search results for &ldquo;{text}&rdquo;:</h3>
-    <ListingWithFilter resources={resources} />
-  </section>);
+  return (
+    <section className="search-result-page">
+      <h3>Search results for &ldquo;{text}&rdquo;:</h3>
+      <ListingWithFilter resources={resources} />
+    </section>
+  );
 };
 
 export default SearchResultsPage;
