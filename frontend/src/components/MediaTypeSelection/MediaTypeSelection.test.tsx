334b35f1e645db2a05808ecb06f173f0da9aec76
Irene PR <ipice1618@protonmail.com>
2022-12-12 12:04:45 +0100

    test that video checkbox works

--- frontend/src/components/MediaTypeSelection/MediaTypeSelection.test.tsx
+++ frontend/src/components/MediaTypeSelection/MediaTypeSelection.test.tsx
@@ -1,11 +1,38 @@
-import TestRenderer from "react-test-renderer";
+import TestRenderer, { act } from "react-test-renderer";
 import MediaTypeSelection from "./MediaTypeSelection";
 import React from "react";
+import { render, screen } from "@testing-library/react";
+import { MediaType } from "../../models/MediaTypes";
 
 test("snapshot test: the html matches", () => {
   const card = TestRenderer.create(
-    <MediaTypeSelection/>,
+    <MediaTypeSelection setFilterMediaTypes={jest.fn()} />,
   );
 
   expect(card.toJSON()).toMatchSnapshot();
+});
+
+describe("Given a media filter selection component", () => {
+  let mockSetFilterMediaTypes = jest.fn();
+  beforeEach(() => {
+    mockSetFilterMediaTypes = jest.fn();
+  });
+
+  describe("When I check the video checkbox", () => {
+    test("Then it updates the state to only show videos", () => {
+      act(() => {
+        render(
+          <MediaTypeSelection setFilterMediaTypes={mockSetFilterMediaTypes} />,
+        );
+      });
+
+      const videoCheckbox = screen.getByRole("checkbox", { name: "video" });
+      act(() => {
+        videoCheckbox.click();
+      });
+      const expectedArray: Array<MediaType> = ["video"];
+
+      expect(mockSetFilterMediaTypes).toBeCalledWith(expectedArray);
+    });
+  });
 });
