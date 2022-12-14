9e7ccc69a8421e95f61cfff2523a2b214712ac4b
tiagobica <tiago.bica@codurance.com>
2022-12-14 14:47:15 +0000

    Enable possible use of testContainers in test.

--- backend/src/test/java/com/codurance/codurawise/domain/services/SearchServiceTest.java
+++ backend/src/test/java/com/codurance/codurawise/domain/services/SearchServiceTest.java
@@ -8,12 +8,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
@@ -26,52 +23,6 @@ public class SearchServiceTest {
   @BeforeEach
   public void setup(){
     searchService = new SearchService(repository);
-  }
-
-
-  @Test
-  public void return_by_title() {
-    List<Resource> resourceList = new ArrayList<>();
-    String title = "scrum";
-
-    given(repository.queryByTitle(title)).willReturn(resourceList);
-
-    List<Resource> result = searchService.getResourceBySearch(title, null);
-
-    assertThat(result).isEqualTo(resourceList);
-  }
-
-  @Test
-  public void return_by_tag() {
-    List<Resource> resourceList = new ArrayList<>();
-    String tag = "agile";
-
-    given(repository.queryByTag(tag)).willReturn(resourceList);
-
-    List<Resource> result = searchService.getResourceBySearch(null, tag);
-
-    assertThat(result).isEqualTo(resourceList);
-  }
-
-  @Test
-  public void return_by_title_and_tag() {
-    List<Resource> resourceList = new ArrayList<>();
-    String title = "scrum";
-    String tag = "agile";
-
-    given(repository.queryBothByTitleAndTag(title, tag)).willReturn(resourceList);
-
-    List<Resource> result = searchService.getResourceBySearch(title, tag);
-
-    assertThat(result).isEqualTo(resourceList);
-  }
-
-  @Test
-  public void throws_exception_for_both_nulls() {
-
-    Exception exception = assertThrows(RuntimeException.class, () -> searchService.getResourceBySearch(null, null));
-    assertThat(exception.getMessage()).isEqualTo(
-      "Must provide either title or tag to query!");
   }
 
   @Test
